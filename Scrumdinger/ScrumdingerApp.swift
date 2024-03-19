07fa7e9d018827ea572e62652b835be21a5b2d63
Aziz Bibitov <azizbibitov11@gmail.com>
2024-03-20 00:51:04 +0500

    Project Created

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Aziz Bibitov on 20.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
